3720b01ecc87bc43815beeb59f5df2d3560bca15
mgonzalez14291 <mgonzalez14291@gmail.com>
2016-06-30 12:46:24 -0500

    Renombrado de variables bravo

--- Sumar/src/sumar/Sumar.java
+++ Sumar/src/sumar/Sumar.java
@@ -11,16 +11,16 @@ public class Sumar {
         //comentario
         Scanner l= new Scanner(System.in);
         int a;
-        int b;
+        int bravo;
         int charlie;
         int delta;
         
         System.out.println("Ingrese un numero:");
         a=l.nextInt();
         System.out.println("Ingrese un numero:");
-        b=l.nextInt();
-        charlie=a+b;
-        delta=a-b;
+        bravo=l.nextInt();
+        charlie=a+bravo;
+        delta=a-bravo;
         System.out.println("La suma es: "+charlie);
                 System.out.println("La resta es: "+delta);
     }
